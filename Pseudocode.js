82c240e0d757b02441e1680d7375f137c9d11a44
Greg Marmolejo <greg_marmolejo@yahoo.com>
2019-12-18 10:21:37 -0600

    my contribution so far.

---
+++ Pseudocode.js
@@ -0,0 +1,14 @@
+//How will we capture button clicks?
+$("#").on("click", function () {
+
+});
+//What will you do with the numbers clicked?
+
+//What will you do when the operator is clicked?
+
+//How will you differntiate numbers from operators?
+
+// How will you know the "value" of a number clicked?
+
+// HHow will you know when a user is done entering the first number?
+.Etc.Etc
